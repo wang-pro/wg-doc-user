6d051f945168be1d3b44751a3d35e81784d00bfc
wang-pro <springioc@yeah.net>
2019-08-23 10:28:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wg-doc-user
---
+++ src/main/java/com/wg/doc/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.wg.doc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
