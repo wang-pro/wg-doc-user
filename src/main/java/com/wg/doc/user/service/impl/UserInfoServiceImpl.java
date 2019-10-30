8c8b8029859073d80aed93de2d7190f6bcb6d319
wangliheng <springioc@yeah.net>
2019-10-30 16:50:37 +0800

    fix

--- src/main/java/com/wg/doc/user/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/wg/doc/user/service/impl/UserInfoServiceImpl.java
@@ -6,8 +6,6 @@ import com.wg.doc.user.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
 
